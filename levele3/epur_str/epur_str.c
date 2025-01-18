3facd593805d2ca745f56b60707f74a6843021a2
the-snowflake-16 <ssnowflake080@gmail.com>
2025-01-18 10:11:28 +0100

    fix some bugs

--- levele3/epur_str/epur_str.c
+++ levele3/epur_str/epur_str.c
@@ -10,18 +10,17 @@ int main(int argc, char *argv[])
             i++;
         while (argv[1][i])
         {
+            if(checker == 1)
+                write(1, " ", 1);
             checker = 0;
+            write(1, &argv[1][i], 1);
+            i++;
             while(argv[1][i] == ' ' || argv[1][i] == '\t')
             {
                 checker = 1;
                 i++;
             }
-            if(checker)
-                write(1, " ", 1);
-            write(1, &argv[1][i], 1);
-            
-            i++;
         }
     }
-    write(1, "\n", 1);
+    // write(1, "\n", 1);
 }
